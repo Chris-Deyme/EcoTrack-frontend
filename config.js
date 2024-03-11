90fee8d94346d22dbc165a450c6c1e9ddf62e1ac
Viviane2111 <schommersv06@gmail.com>
2024-03-11 15:54:52 +0100

    mise à jour de l'affichage du score dans le dashboard et mise à jour en base de données

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  // IP_ADDRESS="http://192.168.1.20:3000"
-  // IP_ADDRESS="http://192.168.1.20:3000"
-  // IP_ADDRESS="http://192.168.1.20:3000"
-  IP_ADDRESS: "http://172.20.10.4:3000",
-};
-
-export default config;
