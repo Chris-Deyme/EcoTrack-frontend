e7205a886cac8ab743cddc3d8ad8a98691473ee5
Rochette Raphaël <rochette.raph@gmail.com>
2024-03-15 11:12:53 +0100

    bug fixed

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ const config = {
   // IP_ADDRESS:"http://192.168.207.4:3000"
   // IP_ADDRESS:"http://192.168.207.213:3000"
   // IP_ADDRESS:"http://172.20.10.13:3000",
->>>>>>> 1b6702786f14eef6d0bef670e76c7f0290aa9918
   // IP_ADDRESS: "http://172.20.10.4:3000",
   IP_ADDRESS: "http://172.20.10.2:3000",
 };
