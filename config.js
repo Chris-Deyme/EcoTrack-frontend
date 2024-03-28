59bde567cd78d8e1f0fae49191896c8955529532
Chris-Deyme <chris.deyme@gmail.com>
2024-03-28 15:57:07 +0100

    deployment IP address

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 const config = {
   // IP_ADDRESS:"http://192.168.1.20:3000"
   // IP_ADDRESS:"http://192.168.207.4:3000"
-  IP_ADDRESS:"http://172.20.10.13:3000",
+  // IP_ADDRESS:"http://172.20.10.13:3000",
   // IP_ADDRESS: "http://172.20.10.4:3000",
-  IP_ADDRESS: "http://172.20.10.2:3000",
+  // IP_ADDRESS: "http://172.20.10.2:3000",
+  IP_ADDRESS: "https://eco-track-backend-nine.vercel.app/",
 };
 
 export default config;
